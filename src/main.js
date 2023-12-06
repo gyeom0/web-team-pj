716b82a4df59141f2bf58ced70c762ca5cb62fa5
gyeom0 <121607278+gyeom0@users.noreply.github.com>
2023-12-07 00:32:22 +0900

    Co-authored-by: Haesung Kang <koh1260@users.noreply.github.com>

--- src/main.js
+++ src/main.js
@@ -19,7 +19,7 @@ for (let i = 0; i < text.length; i++) {
 
 // 타이핑효과
 const inputElement = document.querySelector(".home_serch_input");
-const defaultText = "  한선겸 |";
+const defaultText = "  한 선 겸 |";
 
 function typeText() {
   let currentIndex = 0;
@@ -30,7 +30,7 @@ function typeText() {
     if (currentIndex <= defaultText.length) {
       setTimeout(updateValue, 200); // 200 milliseconds 간격으로 타이핑 효과 반복
     } else {
-      setTimeout(typeText, 4000); // 4초 후에 다시 반복
+      setTimeout(typeText, 3000); // 3초 후에 다시 반복
     }
   }
 
@@ -39,3 +39,105 @@ function typeText() {
 
 // 페이지 로드 시 타이핑 효과 시작
 typeText();
+
+//모달창
+// 버튼과 모달 요소들을 변수로 저장
+const open_btns = document.querySelectorAll(".open_btn");
+const modals = document.querySelectorAll(".modal");
+const close_btns = document.querySelectorAll(".close_btn");
+
+// 각 버튼에 이벤트 리스너 추가
+open_btns.forEach((button) => {
+  button.addEventListener("click", function () {
+    const targetModalId = button.getAttribute("data-modal-target");
+    const targetModal = document.getElementById(targetModalId);
+    openModal(targetModal);
+  });
+});
+
+// 각 닫기 버튼에 이벤트 리스너 추가
+close_btns.forEach((close) => {
+  close.addEventListener("click", function () {
+    const targetModalId = close.getAttribute("data-modal-close");
+    const targetModal = document.getElementById(targetModalId);
+    closeModal(targetModal);
+  });
+});
+
+const overlay = document.querySelector(".overlay");
+// 모달 열기 함수
+function openModal(modal) {
+  modal.style.display = "block";
+  document.body.classList.add("modal-open");
+  overlay.classList.add("active");
+}
+
+// 모달 닫기 함수
+function closeModal(modal) {
+  modal.style.display = "none";
+  document.body.classList.remove("modal-open");
+  overlay.classList.remove("active");
+}
+
+// 모든 닫기 버튼에 이벤트 리스너 추가
+close_btns.forEach(function (close) {
+  close.addEventListener("click", function () {
+    const targetModalId = close.getAttribute("data-modal-close");
+    const targetModal = document.getElementById(targetModalId);
+    closeModal(targetModal);
+  });
+});
+
+//사진넘기기
+const allModals = document.querySelectorAll(".modal");
+
+allModals.forEach((modal, index) => {
+  const slideImages = modal.querySelector(".slide_images");
+  const nextButton = modal.querySelector(".btn_next");
+  const prevButton = modal.querySelector(".btn_prev");
+  const images = modal.querySelectorAll(".draw, .ani, .color");
+  let currentImageIndex = 0;
+
+  updateImage();
+
+  nextButton.addEventListener("click", () => {
+    if (currentImageIndex < images.length - 1) {
+      currentImageIndex++;
+      updateImage();
+    }
+  });
+
+  prevButton.addEventListener("click", () => {
+    if (currentImageIndex > 0) {
+      currentImageIndex--;
+      updateImage();
+    }
+  });
+
+  function updateImage() {
+    images.forEach((image) => (image.style.display = "none"));
+    images[currentImageIndex].style.display = "block";
+    prevButton.disabled = currentImageIndex === 0;
+    nextButton.disabled = currentImageIndex === images.length - 1;
+  }
+});
+
+// arrowUp버튼
+// arrowUp 버튼 표시 여부를 결정하는 함수
+function updateArrowVisibility() {
+  const arrowUp = document.querySelector(".arrow-up");
+  const homeSection = document.getElementById("home");
+  const homeHeight = homeSection.offsetHeight;
+
+  if (window.scrollY > homeHeight / 2) {
+    arrowUp.style.opacity = 1;
+  } else {
+    arrowUp.style.opacity = 0;
+  }
+}
+
+// 스크롤 이벤트에 updateArrowVisibility 함수 연결
+document.addEventListener("scroll", updateArrowVisibility);
+
+// 페이지 로드 시에도 updateArrowVisibility 함수 호출
+document.addEventListener("DOMContentLoaded", updateArrowVisibility);
